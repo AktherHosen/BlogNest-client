cd4e6b1e831b38b49ff6533f3de8cf5418ef4047
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-02 15:16:07 +0600

    Basic router setup with folder structure added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque minus
+        facilis sequi incidunt non, aspernatur unde tempore saepe, commodi dicta
+        rem suscipit optio similique illo delectus autem fugiat cumque provident
+        quam ducimus doloremque laborum! Praesentium omnis nulla iure eveniet
+        mollitia?
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
